672131a6ec8445f80db1c42aeda5ae7cb08fef7d
Stephanie M <stephaniecmayes@gmail.com>
2023-11-04 18:25:11 -0600

    Task: Create recipe model

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+#create recipe model
+class Recipe(models.Model):
+    title = models.TextField(blank=False, help_text="Enter title of recipe.")
+    ingredients = models.TextField(blank=False, help_text="Enter the ingredients.")
+    directions = models.TextField(blank=False, help_text="Enter the directions.")
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('recipe-detail', args=[str(self.id)])
+    
+
+
+
+
